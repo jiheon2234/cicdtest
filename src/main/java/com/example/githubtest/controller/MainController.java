c0a11390f065bbc4bcb506309a925b0eb2884ca4
jiheon2234 <jiheon2234@gmail.com>
2024-02-05 21:48:09 +0900

    inital commit

---
+++ src/main/java/com/example/githubtest/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.githubtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+	public String hello(){
+		return """
+   HELLO WORLD!!
+   abc
+			""";
+	}
+}
